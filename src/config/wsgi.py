c584b5b01a1f106589438123884915b2a1001849
Shapka Oleksii <shapkaoleksiiu@gmail.com>
2025-07-06 14:57:16 +0300

    .

--- src/config/wsgi.py
+++ src/config/wsgi.py
@@ -10,6 +10,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent / "src"))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.config.settings')
 
